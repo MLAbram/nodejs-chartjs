550f6e225d8b8d1568e5986ae3fbb73e367ca617
MLAbram <23126920+MLAbram@users.noreply.github.com>
2023-02-23 17:15:33 -0600

    code updates

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const pool = require('./routes/pg_db');
 const path = require('path');
 const hbs = require('hbs');
 const HTMLFolder = path.join(__dirname, './html');
-const v1_charts = require('./routes/v1_charts');
-const v1_dashboard = require('./routes/v1_dashboard');
+const charts = require('./routes/charts');
+const dashboard = require('./routes/dashboard');
 
 const debug_code = 0;  // 0 = False, 1 = True
 
@@ -16,19 +16,19 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(HTMLFolder));
 app.use(express.urlencoded({extended:false}));
-app.use('/api/v1', v1_charts);
-app.use('/api/v1/dashboard', v1_dashboard);
+app.use('/api/v1', charts);
+app.use('/api/v1/dashboard', dashboard);
 
 // home page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/v1_charts1', (req, res) => {
-  res.render('v1_charts1');
+app.get('/charts1', (req, res) => {
+  res.render('charts1');
 });
 
-app.get('/v1_charts2', (req, res) => {
+app.get('/charts2', (req, res) => {
   pool.query('select domain_t, count(domain_t) cnt from chatbot.activity group by domain_t order by domain_t;', (error, result) => {
     if (error) {
       if (debug_code == 0) {
@@ -41,12 +41,12 @@ app.get('/v1_charts2', (req, res) => {
         const sqlResults = [];
         sqlResults.push(result.rows);
         
-        res.render('v1_charts2', {sqlResults: sqlResults});
+        res.render('charts2', {sqlResults: sqlResults});
       } else {
         if (debug_code == 0) {
           return res.status(400).send('Invalid Credentials...');
         } else {
-          return res.status(400).send('Line: 46\n' + error);
+          return res.status(400).send('Line: 49\n' + error);
         }
       }
     }
